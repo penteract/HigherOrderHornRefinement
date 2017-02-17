6625af25bcb6b848c0f8ce16a64211863a96c0ed
Toby Cathcart Burn <tcathcartburn@gmail.com>
2017-02-17 15:34:10 +0000

    minor changes to tests

--- tests/test.sh
+++ tests/test.sh
@@ -2,6 +2,6 @@
 cat tests/test1.inp | ./Main > tests/test1_2.out
 ./Main -u tests/test1.inp tests/test1_u.out
 ./Main -r tests/test1.inp tests/test1_3.out
-./Main.exe -yr ./tests/test1.inp | z3 -in -smt2 > tests/test1_z.out
-./Main.exe -yr ./tests/test2.inp | z3 -in -smt2 > tests/test2_z.out
+./Main -yr ./tests/test1.inp | z3 -in -smt2 > tests/test1_z.out
+./Main -yr ./tests/test2.inp | z3 -in -smt2 > tests/test2_z.out
 git diff --stat tests
