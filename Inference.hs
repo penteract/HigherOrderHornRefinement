491189ca8db7a769a09571a8f0d61a203cd53399
Toby Cathcart Burn <toby.cathcartburn@pmb.ox.ac.uk>
2017-11-17 23:02:04 +0000

    Minor tidying

--- Inference.hs
+++ Inference.hs
@@ -18,6 +18,8 @@ import Control.Monad.Except(throwError)
 
 import Tools
 
+(↦) = (,)
+
 isFm ::[Variable] -> Term -> Bool -- does not check the sort
 isFm vars (Constant c) = isIlaSymbol c
 isFm vars (Variable x) = x `elem` vars
@@ -53,25 +55,25 @@ infer g (Apply (Constant "∃") (Lambda x Int t)) = do
 infer g t
     | isFm [x | (x,t)<-g , t==IntT] t = return ([], Constant "true", BoolT t)--(Constraint)
 infer g (Apply t1 t2) = do
-    dcty <- infer g t1
-    case dcty of
-         (d1,c1,(ArrowT x IntT ty))-> return
-             (d1,c1,replaceInMT [(x,t2)] ty)--(AppI)
-         (d1,c1,(ArrowT _ ty1 ty))-> do
+    (d1,c1,ty) <- infer g t1
+    case ty of
+         (ArrowT x IntT ty)-> return
+             (d1,c1,replaceInMT [x ↦ t2] ty)--(AppI)
+         (ArrowT _ ty1 ty) -> do
              (d2,c2,ty2) <- infer g t2
              c3 <- inferSub ty2 ty1
              return (d1++d2, aand (aand c1 c2) c3, ty)--(AppR)
          _ -> throwError ("applying term with non-function type\n"++
-                          show dcty ++show t1 ++ show t2++";"++show g)
+                          show (d1,c1,ty) ++show t1 ++ show t2++";"++show g)
 
 infer g (Lambda x Int t) = do
     v <- freshVar
-    (d1,c,ty) <- infer ((v,IntT):g) (replaceInTerm [(x,Variable v)] t)
+    (d1,c,ty) <- infer ((v,IntT):g) (replaceInTerm [x ↦ Variable v] t)
     return (d1,aforall v Int c,ArrowT v IntT ty)--(AbsI)
 infer g (Lambda x s t) = do
     v <- freshVar
     (ty1,d1) <- freshTy (flatenv g) s
-    (d2,c,ty2) <- infer ((v,ty1):g) (replaceInTerm [(x,Variable v)] t)
+    (d2,c,ty2) <- infer ((v,ty1):g) (replaceInTerm [x ↦ Variable v] t)
     return (d1++d2,c,ArrowT "_" ty1 ty2) --(AbsR)
 infer g t = throwError ("bad part of clause " ++ show t)
 
@@ -79,7 +81,7 @@ inferSub :: MonoType -> MonoType -> Mfresh Term
 inferSub (BoolT t1) (BoolT t2) = return $ aimplies t1 t2 --(SubBool)
 inferSub (ArrowT x IntT ty) (ArrowT y IntT ty_) = do
     z <- freshVar
-    c <- inferSub (replaceInMT [(x,Variable z)] ty) (replaceInMT [(y,Variable z)] ty_)
+    c <- inferSub (replaceInMT [x ↦ Variable z] ty) (replaceInMT [y ↦ Variable z] ty_)
     return $ aforall z Int c--(SubI)
 inferSub (ArrowT "_" ty1 ty2) (ArrowT "_" ty1_ ty2_) = do
     c1 <- inferSub ty1_ ty1
